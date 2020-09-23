1e34aaf556d5ec4628c67430d85fa8e1c258f084
Dragifey <sahniquawut@gmail.com>
2020-09-23 08:22:36 +0200

    idk a lot

---
+++ Js/script.js
@@ -0,0 +1,11 @@
+// TweenMax.to(".overlay h1", 2, {
+// 	opacity: 0,
+// 	y: -60,
+// 	ease: Expo.easeInOut
+// 	})
+
+// TweenMax.to(".overlay", 2, {
+// 	delay: 0.7,
+// 	top: "-100%",
+// 	ease: Expo.easeInOut
+// 	})
