b3f9247005013374f73f76aa22583cae5776418c
Dragifey <sahniquawut@gmail.com>
2020-10-07 18:02:19 +0200

    Tons of stuff

--- Js/script.js
+++ Js/script.js
@@ -1,11 +1,17 @@
-// TweenMax.to(".overlay h1", 2, {
-// 	opacity: 0,
-// 	y: -60,
-// 	ease: Expo.easeInOut
-// 	})
+TweenMax.to(".overlay h1", 2, {
+	opacity: 0,
+	y: -60,
+	ease: Expo.easeInOut
+	})
 
-// TweenMax.to(".overlay", 2, {
-// 	delay: 0.7,
-// 	top: "-100%",
-// 	ease: Expo.easeInOut
-// 	})
+TweenMax.to(".overlay img", 2, {
+	opacity: 0,
+	y: -60,
+	ease: Expo.easeInOut
+	})
+
+TweenMax.to(".overlay", 2, {
+	delay: 0.7,
+	top: "-100%",
+	ease: Expo.easeInOut
+	})
